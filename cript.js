5bf15bc90b7b902d271be16590ecb1080eea5473
Valerie Ndanu <valeriendanu8@gmail.com>
2026-06-16 15:12:05 +0300

    Implement client management features
    Added functionality to manage clients including adding and displaying client information.

--- cript.js
+++ cript.js
@@ -1,1 +1,51 @@
-console.log("ISP Dashboard Loaded");
+let clients = [];
+
+function addClient() {
+
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const packagePlan = document.getElementById("package").value;
+
+    if(name === "" || phone === ""){
+        alert("Fill all fields");
+        return;
+    }
+
+    const client = {
+        name,
+        phone,
+        packagePlan,
+        status:"Active"
+    };
+
+    clients.push(client);
+
+    displayClients();
+}
+
+function displayClients(){
+
+    const table = document.getElementById("clientTable");
+
+    table.innerHTML = "";
+
+    clients.forEach(client => {
+
+        table.innerHTML += `
+            <tr>
+                <td>${client.name}</td>
+                <td>${client.phone}</td>
+                <td>${client.packagePlan}</td>
+                <td>${client.status}</td>
+            </tr>
+        `;
+    });
+
+    document.getElementById("totalClients").textContent = clients.length;
+
+    document.getElementById("activeClients").textContent =
+        clients.filter(c => c.status === "Active").length;
+
+    document.getElementById("inactiveClients").textContent =
+        clients.filter(c => c.status === "Inactive").length;
+}
